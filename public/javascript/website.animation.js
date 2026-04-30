468ce5bab7261c7339ca945673b5dfeacfa8360a
RAP <89638989+rapguide-dev@users.noreply.github.com>
2026-04-30 08:26:47 +0800

    added some new files | Rapguide.net

---
+++ public/javascript/website.animation.js
@@ -0,0 +1,20 @@
+// Tab Animation 
+const title = [
+    'Rapguide.net'
+]
+
+let index = 0;
+let Backspace = false;
+
+function animatedTitle() {
+    const titleElement = document.getElementById('animated-title')
+}
+
+// Role Changes Every 3 Seconds Animation
+const roles = [
+    'Frontend Developer',
+    'Backend Developer',
+    'UI/IX Designer',
+    'GTA SA-MP Scripter'
+];
+
