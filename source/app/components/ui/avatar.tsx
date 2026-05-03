f005d6807cc4ebbb6106b03e0cf752df390fcd7b
RAP <89638989+rapguide-dev@users.noreply.github.com>
2026-05-03 23:04:50 +0800

    fixed import { cn } | Rapguide.net

--- source/app/components/ui/avatar.tsx
+++ source/app/components/ui/avatar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
-import { cn } from "./utitilies";
+import { cn } from "./utilities";
 
 function Avatar({
   className,
