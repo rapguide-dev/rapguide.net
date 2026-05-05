d6fc91cf6cd09c0f51b8842a49aaedf681b33688
RAP <89638989+rapguide-dev@users.noreply.github.com>
2026-05-05 11:33:54 +0800

    script dialog.tsx | Rapguide.net

--- source/app/components/ui/alert-dialog.tsx
+++ source/app/components/ui/alert-dialog.tsx
@@ -39,4 +39,19 @@ function AlertDialogOverlay({
         {...props}
       />
     </AlertDialogPortal>
-  );}
+  );
+}
+
+function AlertDialogContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof AlertDialogPrimitive.Content>) {
+  return (
+    <AlertDialogPortal>
+      <AlertDialogOverlay />
+      <AlertDialogPrimitive.Content data-slot="alert-dialog-content" className={cn("bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg", className)}
+        {...props}
+      />
+    </AlertDialogPortal>
+  );
+}
